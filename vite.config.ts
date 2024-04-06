08f5e8e1481d90512564ffc32187a408ddedb597
Averito <avenantmar@gmail.com>
2024-04-06 16:28:57 +0300

    npm publish public

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), viteTsConfigPaths()]
+	plugins: [react(), viteTsConfigPaths(), dts()],
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'src/main.tsx'),
+			name: 'AverlistUI',
+			formats: ['es'],
+			fileName: 'averlist-ui'
+		}
+	}
 })
