781175d1b0489fcd6fb026645afe17fe42c1391e
Averito <avenantmar@gmail.com>
2024-04-09 20:58:02 +0300

    fix: Button.tsx

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,4 @@
-import {
-	memo,
-	MouseEventHandler,
-	NamedExoticComponent,
-	PropsWithChildren
-} from 'react'
+import { FC, MouseEventHandler, PropsWithChildren } from 'react'
 import { ButtonProps } from './Button.types.ts'
 import { Loader } from '@components/Loader'
 import classnames from 'classnames'
@@ -13,44 +8,41 @@ import styles from './Button.module.scss'
  * @description Button Component
  * @type {FC<PropsWithChildren<ButtonProps>>}
  */
-export const Button: NamedExoticComponent<PropsWithChildren<ButtonProps>> =
-	memo(
-		({
-			disabled = false,
-			onClick,
-			className,
-			size = 'medium',
-			loading = false,
-			children = 'Button'
-		}) => {
-			const onClickWrapper: MouseEventHandler<HTMLButtonElement> = event => {
-				if (loading) {
-					return
-				}
-				onClick?.(event)
-			}
-
-			return (
-				<button
-					type='button'
-					className={classnames(
-						styles.averui_button,
-						styles[`averui_button--${size}`],
-						{ [styles[`averui_button--loading`]]: loading },
-						className
-					)}
-					onClick={onClickWrapper}
-					disabled={disabled}
-				>
-					{children}
-					{loading && (
-						<>
-							{size === 'small' && <Loader color='#fff' size={14} />}
-							{size === 'medium' && <Loader color='#fff' size={16} />}
-							{size === 'large' && <Loader color='#fff' size={18} />}
-						</>
-					)}
-				</button>
-			)
+export const Button: FC<PropsWithChildren<ButtonProps>> = ({
+	disabled = false,
+	onClick,
+	className,
+	size = 'medium',
+	loading = false,
+	children = 'Button'
+}) => {
+	const onClickWrapper: MouseEventHandler<HTMLButtonElement> = event => {
+		if (loading) {
+			return
 		}
+		onClick?.(event)
+	}
+
+	return (
+		<button
+			type='button'
+			className={classnames(
+				styles.averui_button,
+				styles[`averui_button--${size}`],
+				{ [styles[`averui_button--loading`]]: loading },
+				className
+			)}
+			onClick={onClickWrapper}
+			disabled={disabled}
+		>
+			{children}
+			{loading && (
+				<>
+					{size === 'small' && <Loader color='#fff' size={14} />}
+					{size === 'medium' && <Loader color='#fff' size={16} />}
+					{size === 'large' && <Loader color='#fff' size={18} />}
+				</>
+			)}
+		</button>
 	)
+}
